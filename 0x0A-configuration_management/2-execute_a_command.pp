58a2951c9bf1ed454dfa360f366f80fac62262c8
EstephaniaCalvoC <2177@holbertonschool.com>
2021-04-03 16:21:01 -0500

    Add 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,1 +1,6 @@
-#!/usr/bin/env bash
+# Creat a manifest that kills a process named killmenow.
+
+exec { 'otros':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin/', '/usr/loca/bin/', '/bin/'],
+}
