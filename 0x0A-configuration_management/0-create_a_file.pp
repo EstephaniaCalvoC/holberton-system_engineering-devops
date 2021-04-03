00fed2bce339b36d6eba50d0af48bd79eabc7711
EstephaniaCalvoC <2177@holbertonschool.com>
2021-04-03 15:56:08 -0500

    Add 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+# Create a file in /tmp.
+
+file { 'holberton':
+  path => '/tmp/holberton',
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+}
