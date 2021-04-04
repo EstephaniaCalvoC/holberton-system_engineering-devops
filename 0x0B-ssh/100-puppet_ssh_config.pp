d4bf08490c4f3de247573dc943f5d29d434ac9d0
EstephaniaCalvoC <2177@holbertonschool.com>
2021-04-03 20:38:21 -0500

    User stdlib

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
 # Configurate SSH to connect to a server without typing a password.
 
+include stdlib
+
 file_line { 'Turn off passwd auth':
-  path     => '/etc/ssh/ssh_config',
-  line     => 'PasswordAuthentication yes',
-  match    => '#   PasswordAuthentication yes',
+  path =>  '/etc/ssh/ssh_config',
+  line =>  'PasswordAuthentication no',
 }
 
-file_line { 'JuanMa':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/holberton',
+file_line {  'Declare identity file':
+  path =>  '/etc/ssh/ssh_config',
+  line =>  'IdentityFile ~/.ssh/holberton',
 }
