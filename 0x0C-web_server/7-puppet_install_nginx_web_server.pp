938f14e096c942f27bf68a6388433b2b8a37407a
EstephaniaCalvoC <2177@holbertonschool.com>
2021-04-06 19:36:54 -0500

    Fix style

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,20 +4,20 @@
 # The redirection is “301 Moved Permanently”
 
 package {'install':
-	ensure => installed,
-	name => 'nginx',
+  ensure => installed,
+  name   => 'nginx',
 }
 file {'root':
-	path => '/var/www/html/index.html',
-	content => 'Holberton School',	
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School',
 }
 file_line {'redirect':
-	path => '/etc/nginx/sites-available/default',
-	ensure => present,
-	after => 'server_nme_;',
-	line => 'rewrite ^/redirect_me https://github.com/EstephaniaCalvoC/ permanent;',
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_nme_;',
+  line   => 'rewrite ^/redirect_me https://github.com/EstephaniaCalvoC/ permanent;',
 }
 service {'run':
-	ensure => running,
-	require => Package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
