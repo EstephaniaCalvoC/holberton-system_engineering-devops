c8513450d59e04c7b52462dd9259b2744d09c245
EstephaniaCalvoC <2177@holbertonschool.com>
2021-05-03 12:21:09 -0500

    Add 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Return information about his/her TODO list progress"""
+import restquests
+import argv from sys
+
+
+if __name__ == "__main__":
+    user_id = argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/"
+    uri_user_id = "users/{}".format(user_id)
+    uri_todos = "todos"
+
+    user = requests.get(url + uri_user_id).json()
+    tasks = requests.get(url + uri_todos, params={"userId": user_id}).json()
+
+    tasks_completed = [task for task in tasks if task.get("completed") is True]
+
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(tasks_coompleted),
+                                                          len(taks))))
+
+    [print("\t{}".format(task.get("title"))) for task in tasks_completed]
