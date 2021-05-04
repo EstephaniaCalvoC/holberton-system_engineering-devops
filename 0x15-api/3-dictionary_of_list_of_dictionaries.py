45e7328ce7b36e3a79a075b22cfbb87b81f54b29
EstephaniaCalvoC <2177@holbertonschool.com>
2021-05-03 21:48:20 -0500

    Fix logic

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -10,14 +10,17 @@ if __name__ == "__main__":
     uri_todos = "todos"
 
     users = requests.get(url + uri_user).json()
+    dir_user_tasks = {}
+
+    for user in users:
+        user_id = user.get("id")
+        user_name = user.get("username")
+        tasks = requests.get(url + uri_todos,
+                             params={"userId": user_id}).json()
+        user_tasks = ({user_id: [{"task": task.get("title"),
+                                  "completed": task.get("completed"),
+                                  "username": user_name} for task in tasks]})
+        dir_user_tasks.update(user_tasks)
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        for user in users:
-            user_id = user.get("id")
-            user_name = user.get("username")
-            tasks = requests.get(url + uri_todos,
-                                 params={"userId": user_id}).json()
-            user_tasks = {user_id: [{"task": task.get("title"),
-                                     "completed": task.get("completed"),
-                                     "username": user_name} for task in tasks]}
-            json.dump(user_tasks, jsonfile)
+        json.dump(dir_user_tasks, jsonfile)
