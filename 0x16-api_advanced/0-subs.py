276e5339aac30c53ba3cf8493ac5e88554f2979b
EstephaniaCalvoC <2177@holbertonschool.com>
2021-05-17 14:22:33 -0500

    Add 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Define number_of_subscribers function"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Query the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+    - If an invalid subreddit is given, the function should return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x016.project:v0 (by /u/ecalvoc"
+        }
+    subreddit_data = requests.get(url,
+                                  headers=headers,
+                                  allow_redirects=False).json().get("data")
+    if subreddit_data:
+        return subreddit_data.get("subscribers")
+    return 0
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -18,12 +18,11 @@ What I learned from this project:
 ---
 ## Tasks
 
-<!--
 * **0. How many subs?**
   * [0-subs.py](./0-subs.py): Python function that returns the total number of
   subscribers for a given subreddit.
   * Returns `0` if an invalid subreddit is given.
-
+<!--
 * **1. Top Ten**
   * [1-top_ten.py](./1-top_ten.py): Python function that prints the top ten
   hottest posts for a given subreddit.
