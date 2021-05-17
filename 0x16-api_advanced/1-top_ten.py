ccb28029ff0a0c67e70509df8d15164dfc57994b
EstephaniaCalvoC <2177@holbertonschool.com>
2021-05-17 18:04:21 -0500

    Add 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Define top_ten function"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Query the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x016.project:v1.0.0 (by /u/ecalvoc)"
+        }
+    params = {"limit": 10}
+    top_data = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False).json().get("data")
+    if top_data:
+        childrens = top_data.get("children")
+        [print(children.get("data").get("title")) for children in childrens]
+        return
+    print("None")
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -22,12 +22,12 @@ What I learned from this project:
   * [0-subs.py](./0-subs.py): Python function that returns the total number of
   subscribers for a given subreddit.
   * Returns `0` if an invalid subreddit is given.
-<!--
+
 * **1. Top Ten**
   * [1-top_ten.py](./1-top_ten.py): Python function that prints the top ten
   hottest posts for a given subreddit.
   * Prints `None` if an invalid subreddit is given.
-
+<!--
 * **2. Recurse it!**
   * [2-recurse.py](./2-recurse.py): Python function that recursively returns a
   list of titles for all hot articles on a given subreddit.
