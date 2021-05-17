defe2e4d66ea0b97bd751a2b745f0e7c43d54e7d
EstephaniaCalvoC <2177@holbertonschool.com>
2021-05-17 18:55:27 -0500

    Add 2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+"""Define recurse function"""
+import requests
+
+
+def get_title(children):
+    """Return children's title"""
+    return children.get("data").get("title")
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Queries the Reddit API and returns a list containing the titles of
+    all hot articles for a given subreddit.
+    - If no results are found for the given subreddit,
+    the function should return None
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x016.project:v1.0.0 (by /u/ecalvoc)"
+        }
+    params = {"limit": 100}
+    if after:
+        params["after"] = after
+
+    hot_data = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False).json().get("data")
+    if not hot_data:
+        return
+
+    childrens = hot_data.get("children")
+    hot_list.extend(list(map(get_title, childrens)))
+
+    after = hot_data.get("after")
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -27,12 +27,12 @@ What I learned from this project:
   * [1-top_ten.py](./1-top_ten.py): Python function that prints the top ten
   hottest posts for a given subreddit.
   * Prints `None` if an invalid subreddit is given.
-<!--
+
 * **2. Recurse it!**
   * [2-recurse.py](./2-recurse.py): Python function that recursively returns a
   list of titles for all hot articles on a given subreddit.
   * Returns `None` if no results are found on the given subreddit.
-
+<!--
 * **3. Count it!**
   * [100-count.py](./100-count.py): Python function that recursively prints a
   sorted count of given keywords parsed from titles of all hot articles on a given
