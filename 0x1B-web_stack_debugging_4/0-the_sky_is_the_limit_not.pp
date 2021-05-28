59873e76a8982bc4c45302edce13114ec676cf8f
EstephaniaCalvoC <2177@holbertonschool.com>
2021-05-27 21:34:24 -0500

    Validate config file existing

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,6 +2,7 @@
 
 # Increse ulimit value
 exec { 'fix-config-nginx':
+  onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
